c1b81ec7274cd476d130ad2829d87cfe2185bea4
stillefront <alexej@stillefront.de>
2019-12-10 10:59:07 +0100

    interface Set created. Class mySet with interface method stubs created.

---
+++ lab5_1/lab5_1/Set.java
@@ -0,0 +1,12 @@
+package lab5_1;
+
+public interface Set<T>{
+
+    public int size();
+    public void add();
+    public void addAll();
+    public void remove();
+    public boolean isElement();
+    public void intersection();
+
+}
---
+++ lab5_1/lab5_1/mySet.java
@@ -0,0 +1,33 @@
+package lab5_1;
+
+public class mySet implements Set {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void add() {
+
+    }
+
+    @Override
+    public void addAll() {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public boolean isElement() {
+        return false;
+    }
+
+    @Override
+    public void intersection() {
+
+    }
+}
